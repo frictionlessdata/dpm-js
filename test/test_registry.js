291f29e751d0dbb45233555c975aad16f025d62d
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-12-31 14:56:43 -0500

    test now use cloudant registry + split to close #13

--- test/test_registry.js
+++ test/test_registry.js
@@ -3,6 +3,7 @@ var util = require('util')
   , clone = require('clone')
   , temp = require('temp')
   , assert = require('assert')
+  , request = require('request')
   , Dpm = require('..')
   , readdirpSync = require('fs-readdir-recursive')
   , difference = require('lodash.difference')
@@ -12,12 +13,13 @@ temp.track();
 
 var root = path.dirname(__filename);
 
+
 describe('dpm', function(){
 
   var conf = {
     protocol: 'http',
-    port: 3000,
-    hostname: 'localhost',
+    port: 80,
+    hostname: 'registry.standardanalytics.io',
     strictSSL: false,
     sha:true,
     name: "user_a",
@@ -25,10 +27,9 @@ describe('dpm', function(){
     password: "user_a"
   };
 
-  var nano = require('nano')('http://seb:seb@127.0.0.1:5984'); //connect as admin
-  var _users = nano.db.use('_users')
-    , registry = nano.db.use('registry');
-
+  function rurl(path){
+    return conf.protocol + '://' + conf.hostname + ((conf.port !== 80) ? ':' + conf.port: '') + path;
+  };
 
   function rm(db, id, cb){
     db['head'](id, function(err, _, headers){
@@ -40,10 +41,10 @@ describe('dpm', function(){
   };
 
   function rmFixtures (cb){
-    rm(registry, 'mydpkg@0.0.0', function(){
-      rm(registry, 'req@0.0.0', function(){
-        rm(_users,  'org.couchdb.user:user_a', function(){
-          rm(_users, 'org.couchdb.user:user_b', function(){            
+    request.del( { url: rurl('/mydpkg-test'), auth: {user: conf.name, pass: conf.password} }, function(err, resp, body){
+      request.del( { url: rurl('/req-test'), auth: {user: conf.name, pass: conf.password} }, function(err, resp, body){
+        request.del( { url: rurl('/rmuser/' + conf.name), auth: {user: conf.name, pass: conf.password} }, function(err, resp, body){
+          request.del( { url: rurl('/rmuser/user_b'), auth: {user: 'user_b', pass: conf.password} }, function(err, resp, body){
             cb();
           });
         });
@@ -56,8 +57,8 @@ describe('dpm', function(){
     var dpm1, dpm2;
 
     before(function(done){
-      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'mydpkg'));
-      dpm2 = new Dpm(conf, path.join(root, 'fixtures', 'require'));
+      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'mydpkg-test'));
+      dpm2 = new Dpm(conf, path.join(root, 'fixtures', 'req-test'));
 
       dpm1.adduser(function(err, headers){
         done()
@@ -66,7 +67,7 @@ describe('dpm', function(){
     
     it('should publish a data package with attachments and raise an error with code if the dpkg is republished', function(done){
       dpm1.publish(function(err, id){     
-        assert.equal('mydpkg@0.0.0', id);
+        assert.equal('mydpkg-test@0.0.0', id);
         dpm1.publish(function(err, id){     
           assert.equal(err.code, 409);
           done();
@@ -76,7 +77,7 @@ describe('dpm', function(){
 
     it('should publish a data package without attachments', function(done){
       dpm2.publish(function(err, id){
-        assert.equal('req@0.0.0', id);
+        assert.equal('req-test@0.0.0', id);
         done();
       });
     });
@@ -91,7 +92,7 @@ describe('dpm', function(){
     var dpm1;
 
     before(function(done){
-      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'mydpkg'));
+      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'mydpkg-test'));
       dpm1.adduser(function(err, headers){
         dpm1.publish(function(err, id){
           done();
@@ -100,7 +101,7 @@ describe('dpm', function(){
     });
     
     it('should unpublish a dpkg', function(done){
-      dpm1.unpublish('mydpkg', function(err, res){
+      dpm1.unpublish('mydpkg-test', function(err, res){
         assert.deepEqual(res, {ok:true});
         done();
       });
@@ -119,16 +120,16 @@ describe('dpm', function(){
     var dpm1, dpm2;
 
     var expected = { 
-      name: 'req',
+      name: 'req-test',
       description: 'a test for require',
-      dataDependencies: { mydpkg: '0.0.0' },
+      dataDependencies: { 'mydpkg-test': '0.0.0' },
       version: '0.0.0',
       keywords: [ 'test', 'datapackage' ],
       resources: [
         {
           name: 'azerty',
-          require: { datapackage: 'mydpkg', resource: 'csv1', fields: ['a'] },
-          url: 'http://localhost:3000/mydpkg/0.0.0/csv1',
+          require: { datapackage: 'mydpkg-test', resource: 'csv1', fields: ['a'] },
+          url: rurl('/mydpkg-test/0.0.0/csv1'),
           format: 'csv',
           schema: { fields: [ { name: 'a', type: 'integer' } ] }
         } 
@@ -136,8 +137,8 @@ describe('dpm', function(){
     };
 
     before(function(done){
-      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'mydpkg'));
-      dpm2 = new Dpm(conf, path.join(root, 'fixtures', 'require'));
+      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'mydpkg-test'));
+      dpm2 = new Dpm(conf, path.join(root, 'fixtures', 'req-test'));
 
       dpm1.adduser(function(err, headers){
         dpm1.publish(function(err, id){
@@ -156,14 +157,14 @@ describe('dpm', function(){
     });
 
     it('should cat the dpkg having resolved the require', function(done){
-      dpm2.cat('req@0.0.0', function(err, dpkg){
+      dpm2.cat('req-test@0.0.0', function(err, dpkg){
         assert.deepEqual(expected, dpkg);
         done();
       });
     });
 
     it('should cat the latest dpkg having resolved the require when version is not specified', function(done){
-      dpm2.cat('req', function(err, dpkg){
+      dpm2.cat('req-test', function(err, dpkg){
         assert.deepEqual(expected, dpkg);
         done();
       });
@@ -171,7 +172,7 @@ describe('dpm', function(){
 
 
     it('should cat the dpkg as is', function(done){
-      dpm2.cat('req@0.0.0', {clone:true}, function(err, dpkg){
+      dpm2.cat('req-test@0.0.0', {clone:true}, function(err, dpkg){
         var exp = clone(expected);
         delete exp.resources[0].schema;
         delete exp.resources[0].url;
@@ -197,10 +198,10 @@ describe('dpm', function(){
 
     var dpm1, dpm2;
     before(function(done){
-      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'require'));
+      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'req-test'));
       var conf2 = clone(conf);
       conf2.name = 'user_b';
-      dpm2 = new Dpm(conf2, path.join(root, 'fixtures', 'require'));
+      dpm2 = new Dpm(conf2, path.join(root, 'fixtures', 'req-test'));
 
       dpm1.adduser(function(err, headers){
         dpm2.adduser(function(err, id){
@@ -212,18 +213,18 @@ describe('dpm', function(){
     });
     
     it('should list the maintainers', function(done){
-      dpm1.lsOwner('req', function(err, maintainers){
+      dpm1.lsOwner('req-test', function(err, maintainers){
         assert.deepEqual(maintainers, expected.slice(0, 1));
         done();
       });    
     });
 
     it('should add a maintainer then remove it', function(done){
-      dpm1.addOwner({username: 'user_b', dpkgName: 'req'}, function(err){
-        dpm1.lsOwner('req', function(err, maintainers){
+      dpm1.addOwner({username: 'user_b', dpkgName: 'req-test'}, function(err){
+        dpm1.lsOwner('req-test', function(err, maintainers){
           assert.deepEqual(maintainers, expected);
-          dpm1.rmOwner({username: 'user_b', dpkgName: 'req'}, function(err){
-            dpm1.lsOwner('req', function(err, maintainers){
+          dpm1.rmOwner({username: 'user_b', dpkgName: 'req-test'}, function(err){
+            dpm1.lsOwner('req-test', function(err, maintainers){
               assert.deepEqual(maintainers, expected.slice(0, 1));
               done();
             });
@@ -233,7 +234,7 @@ describe('dpm', function(){
     });
 
     it("should err", function(done){
-      dpm1.addOwner({username: 'user_c', dpkgName: 'req'}, function(err){
+      dpm1.addOwner({username: 'user_c', dpkgName: 'req-test'}, function(err){
         assert.equal(err.code, 404);
         done();
       })      
@@ -249,10 +250,10 @@ describe('dpm', function(){
   describe('files', function(){
     var dpm1, dpm2;
     before(function(done){
-      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'require'));
+      dpm1 = new Dpm(conf, path.join(root, 'fixtures', 'req-test'));
       dpm1.adduser(function(err, headers){
         dpm1.publish(function(err, body){
-          dpm2 = new Dpm(conf, path.join(root, 'fixtures', 'mydpkg'));
+          dpm2 = new Dpm(conf, path.join(root, 'fixtures', 'mydpkg-test'));
           dpm2.publish(function(err, body){
             done();
           });
@@ -260,11 +261,11 @@ describe('dpm', function(){
       });
     });
 
-    it('should get req@0.0.0', function(done){
+    it('should get req-test@0.0.0', function(done){
       temp.mkdir('test-dpm2-', function(err, dirPath) {
         var dpm = new Dpm(conf, dirPath);
-        dpm.get('req@0.0.0', function(err){
-          fs.readdir(path.join(dirPath, 'req'), function(err, files){
+        dpm.get('req-test@0.0.0', function(err){
+          fs.readdir(path.join(dirPath, 'req-test'), function(err, files){
             assert.deepEqual(files, ['package.json']);
             done();
           });
@@ -272,52 +273,52 @@ describe('dpm', function(){
       });    
     });
 
-    it('should get req@0.0.0 and cache data including the data of the require', function(done){
+    it('should get req-test@0.0.0 and cache data including the data of the require', function(done){
       temp.mkdir('test-dpm2-', function(err, dirPath) {
         var dpm = new Dpm(conf, dirPath);
-        dpm.get('req@0.0.0', {cache: true}, function(err){
-          var files = readdirpSync(path.join(dirPath, 'req'));
+        dpm.get('req-test@0.0.0', {cache: true}, function(err){
+          var files = readdirpSync(path.join(dirPath, 'req-test'));
           assert(files.length && difference(files, [path.join('data', 'azerty.csv'), 'package.json']).length === 0);
           done();
         });
       });
     });
 
-    it('should clone mydpkg', function(done){
+    it('should clone mydpkg-test', function(done){
       temp.mkdir('test-dpm2-', function(err, dirPath) {
         var dpm = new Dpm(conf, dirPath);
-        dpm.clone('mydpkg@0.0.0', function(err){
-          var files = readdirpSync(path.join(dirPath, 'mydpkg'));
+        dpm.clone('mydpkg-test@0.0.0', function(err){
+          var files = readdirpSync(path.join(dirPath, 'mydpkg-test'));
           assert(files.length && difference(files, ['package.json', path.join('scripts', 'test.r'),'x1.csv','x2.csv']).length === 0);                 
           done();
         });
       });
     });
 
-    it('should install req and mydpkg', function(done){
+    it('should install req-test and mydpkg-test', function(done){
       temp.mkdir('test-dpm2-', function(err, dirPath) {
         var dpm = new Dpm(conf, dirPath);
-        dpm.install(['mydpkg@0.0.0', 'req@0.0.0'], function(err){
+        dpm.install(['mydpkg-test@0.0.0', 'req-test@0.0.0'], function(err){
           var files = readdirpSync(path.join(dirPath));
-          assert(files.length && difference(files, [ path.join('data_modules',  'mydpkg', 'package.json'), path.join('data_modules',  'req', 'package.json') ]).length === 0);
+          assert(files.length && difference(files, [ path.join('data_modules',  'mydpkg-test', 'package.json'), path.join('data_modules',  'req-test', 'package.json') ]).length === 0);
           done();
         });
       });
     });
 
-    it('should install req and mydpkg and cache the deps', function(done){
+    it('should install req-test and mydpkg-test and cache the deps', function(done){
       temp.mkdir('test-dpm2-', function(err, dirPath) {
         var dpm = new Dpm(conf, dirPath);
-        dpm.install(['mydpkg@0.0.0', 'req@0.0.0'], {cache:true}, function(err){
+        dpm.install(['mydpkg-test@0.0.0', 'req-test@0.0.0'], {cache:true}, function(err){
           var files = readdirpSync(path.join(dirPath));
 
           var expf = [
-            path.join('data_modules', 'mydpkg', 'data', 'csv1.csv'),
-            path.join('data_modules', 'mydpkg', 'data', 'csv2.csv'),
-            path.join('data_modules', 'mydpkg', 'data', 'inline.json'),
-            path.join('data_modules', 'mydpkg', 'package.json'),
-            path.join('data_modules', 'req', 'data', 'azerty.csv'),
-            path.join('data_modules', 'req', 'package.json')
+            path.join('data_modules', 'mydpkg-test', 'data', 'csv1.csv'),
+            path.join('data_modules', 'mydpkg-test', 'data', 'csv2.csv'),
+            path.join('data_modules', 'mydpkg-test', 'data', 'inline.json'),
+            path.join('data_modules', 'mydpkg-test', 'package.json'),
+            path.join('data_modules', 'req-test', 'data', 'azerty.csv'),
+            path.join('data_modules', 'req-test', 'package.json')
           ];
 
           assert(files.length && difference(files, expf).length === 0);
@@ -326,9 +327,9 @@ describe('dpm', function(){
       });
     });
 
-    it('should resolve the dataDependencies of req', function(done){
-      dpm1.resolveDeps({'req': '*'}, function(err, versions){
-        assert.deepEqual(versions, ['req@0.0.0']);
+    it('should resolve the dataDependencies of req-test', function(done){
+      dpm1.resolveDeps({'req-test': '*'}, function(err, versions){
+        assert.deepEqual(versions, ['req-test@0.0.0']);
         done();
       });
     });
